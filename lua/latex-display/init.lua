fc6eb2a7fc7740a07d9ee49431331b159ab2249a
drewdunning21 <drewdunning21@gmail.com>
2020-09-06 23:21:14 -0700

    messed with some buffer things

--- lua/latex-display/init.lua
+++ lua/latex-display/init.lua
@@ -3,7 +3,8 @@ local function displayPdf(viewer)
     local viewer = vim.g['default_pdf_viewer']
     local dir = vim.fn.expand('%:h')
     local fileName = vim.fn.expand('%:t:r')
-    vim.api.nvim_command('tabnew')
+    vim.api.nvim_command('tab new')
+    -- local bufh = vim.api.nvim_create_buf(
     vim.api.nvim_command('term')
     vim.api.nvim_command('call nvim_paste("cd '.. dir..'\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("pdflatex '.. fileName .. '.tex\n", 1 ,-1)')
--- plugin/latex-display.vim
+++ plugin/latex-display.vim
@@ -4,6 +4,7 @@ endif
 fun! LatexDisplay()
     lua for k in pairs(package.loaded) do if k:match("^latex%-display") then package.loaded[k] = nil end end
     lua require("latex-display").displayPdf()
+    edit #
 endfun
 
 nmap dsp :call LatexDisplay()<CR>
