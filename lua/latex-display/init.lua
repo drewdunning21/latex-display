2629a52bba018330db71cc2176fb55006536fff2
drewdunning21 <drewdunning21@gmail.com>
2020-09-06 22:38:42 -0700

    added custom pdf viewing from custom app but evince is default

--- lua/latex-display/init.lua
+++ lua/latex-display/init.lua
@@ -1,4 +1,6 @@
-local function displayPdf()
+
+local function displayPdf(viewer)
+    local viewer = vim.g['default_pdf_viewer']
     local dir = vim.fn.expand('%:h')
     local fileName = vim.fn.expand('%:t:r')
     vim.api.nvim_command('tabnew')
@@ -6,7 +8,7 @@ local function displayPdf()
     vim.api.nvim_command('call nvim_paste("cd '.. dir..'\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("pdflatex '.. fileName .. '.tex\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("rm *.log *.aux\n", 1 ,-1)')
-    vim.api.nvim_command('call nvim_paste("evince '.. fileName .. '.pdf\n", 1 ,-1)')
+    vim.api.nvim_command('call nvim_paste("' .. viewer .. ' '.. fileName .. '.pdf\n", 1 ,-1)')
     vim.api.nvim_command('tabprev')
 end
 
--- plugin/latex-display.vim
+++ plugin/latex-display.vim
@@ -1,9 +1,10 @@
+if !exists("g:default_pdf_viewer")
+    let g:default_pdf_viewer = 'evince'
+endif
 fun! LatexDisplay()
-    " dont forget to remove this one....
-    lua for k in pairs(package.loaded) do if k:match("^latex%-display") then package.loaded[k] = nil end end
+    " lua for k in pairs(package.loaded) do if k:match("^latex%-display") then package.loaded[k] = nil end end
     lua require("latex-display").displayPdf()
 endfun
-
 
 augroup LatexDisplay
     autocmd!
