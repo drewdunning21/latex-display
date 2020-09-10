18557a729c8f58a9e58d1615f0c7eb6fd9f3556a
drewdunning21 <drewdunning21@gmail.com>
2020-09-10 11:20:07 -0700

    removed display on save

--- lua/latex-display/init.lua
+++ lua/latex-display/init.lua
@@ -1,18 +1,37 @@
 
-local function displayPdf(viewer)
+local function displayPdf()
     local viewer = vim.g['default_pdf_viewer']
     local dir = vim.fn.expand('%:h')
     local fileName = vim.fn.expand('%:t:r')
     vim.api.nvim_command('tab new')
-    -- local bufh = vim.api.nvim_create_buf(
     vim.api.nvim_command('term')
     vim.api.nvim_command('call nvim_paste("cd '.. dir..'\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("pdflatex '.. fileName .. '.tex\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("rm *.log *.aux\n", 1 ,-1)')
     vim.api.nvim_command('call nvim_paste("' .. viewer .. ' '.. fileName .. '.pdf\n", 1 ,-1)')
-    vim.api.nvim_command('tabprev')
+    vim.api.nvim_command('tabclose')
+    vim.api.nvim_command('stopinsert')
 end
 
+local function onSave()
+    local viewer = vim.g['default_pdf_viewer']
+    local dir = vim.fn.expand('%:h')
+    local fileName = vim.fn.expand('%:t:r')
+    -- vim.api.nvim_command('tab new')
+    vim.api.nvim_command('term')
+    vim.api.nvim_command('call nvim_paste("cd '.. dir..'\n", 1 ,-1)')
+    vim.api.nvim_command('call nvim_paste("pdflatex '.. fileName .. '.tex\n", 1 ,-1)')
+    vim.api.nvim_command('call nvim_paste("rm *.log *.aux\n", 1 ,-1)')
+    vim.api.nvim_command('call nvim_paste("' .. viewer .. ' '.. fileName .. '.pdf\n", 1 ,-1)')
+    -- vim.api.nvim_command('bd!')
+    print('ere')
+    -- vim.api.nvim_command('edit #')
+    -- vim.api.nvim_command('tabclose')
+    vim.api.nvim_command('stopinsert')
+end
+
+
 return {
-    displayPdf = displayPdf
+    displayPdf = displayPdf,
+    onSave = onSave
 }
--- plugin/latex-display.vim
+++ plugin/latex-display.vim
@@ -1,14 +1,20 @@
 if !exists("g:default_pdf_viewer")
     let g:default_pdf_viewer = 'evince'
 endif
+if !exists("g:display_on_edit")
+    let g:display_on_edit = 1
+endif
+
 fun! LatexDisplay()
     lua for k in pairs(package.loaded) do if k:match("^latex%-display") then package.loaded[k] = nil end end
     lua require("latex-display").displayPdf()
-    edit #
 endfun
 
 nmap dsp :call LatexDisplay()<CR>
 
 augroup LatexDisplay
     autocmd!
+    if g:display_on_edit
+        " autocmd BufWrite *.tex :lua require("latex-display").onSave()
+    endif
 augroup END
